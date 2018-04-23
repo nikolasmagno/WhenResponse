268c0691dec9d8493a4d5bf6369e29be6614dda6
Nikolas Magno <nikolasnyo@hotmail.com>
2018-04-23 20:13:35 -0300

    initializing

---
+++ ResponseWhen.RestSharp/Extension.cs
@@ -0,0 +1,16 @@
+﻿namespace ResponseWhen.RestSharp
+{
+    using global::RestSharp;
+    using System;
+
+    public static class Extension
+    {
+        public static IRestResponse When(this IRestResponse response, Func<IRestResponse,bool> condition, Action<IRestResponse> action)
+        {
+            if (condition(response))
+                action(response);
+
+            return response;
+        }
+    }
+}
