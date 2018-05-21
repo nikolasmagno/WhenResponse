8add512b98d641ff59ffd19293539a0d543432f4
Nikolas Magno <nikolasnyo@hotmail.com>
2018-05-21 20:37:26 -0300

    Adicionando pacotes nuget

---
+++ WhenResponse.RestSharp.NetCore/Extension.cs
@@ -0,0 +1,16 @@
+﻿namespace WhenResponse.RestSharp
+{
+    using global::RestSharp;
+    using System;
+
+    public static class Extension
+    {
+        public static IRestResponse When(this IRestResponse response, Func<IRestResponse,bool> condition, Action<IRestResponse> action)
+        {
+            if (condition(response))
+                action(response);
+
+            return response;
+        }
+    }
+}
