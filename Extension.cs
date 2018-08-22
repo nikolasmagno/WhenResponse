726712350977575ca8f39a7f970f2ab17f9b22c6
Nikolas Magno Braga Leite - AeC <nikolas.leite@grupoaec.com.br>
2018-08-22 16:56:41 -0300

    Alterando para .netcore

--- Extension.cs
+++ Extension.cs
@@ -1,11 +1,11 @@
 ﻿namespace WhenResponse.RestSharp
 {
-    using global::RestSharp;
     using System;
+    using global::RestSharp;
 
     public static class Extension
     {
-        public static IRestResponse When(this IRestResponse response, Func<IRestResponse,bool> condition, Action<IRestResponse> action)
+        public static IRestResponse When(this IRestResponse response, Func<IRestResponse, bool> condition, Action<IRestResponse> action)
         {
             if (condition(response))
                 action(response);
